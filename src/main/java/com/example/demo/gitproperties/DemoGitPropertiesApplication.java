a30e4af90f0cd5c9ea08a3bbdbfd349b294d0a02
neild <neilddixon@gmail.com>
2021-06-21 19:20:37 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/gitproperties/DemoGitPropertiesApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.gitproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitPropertiesApplication.class, args);
+    }
+
+
+}
